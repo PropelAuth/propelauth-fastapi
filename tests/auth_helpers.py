701f95d00068c1167c15331c79f477e07fa5bad0
andrew-propelauth <89474256+andrew-propelauth@users.noreply.github.com>
2023-04-21 12:57:10 -0700

    Update dependency on propelauth-py for metadata changes (#3)
    * Update dependency on propelauth-py for metadata changes
    
    * Fix build by switching to ubuntu-latest

--- tests/auth_helpers.py
+++ tests/auth_helpers.py
@@ -9,6 +9,7 @@ from tests.conftest import BASE_AUTH_URL
 def create_access_token(user, private_key_pem, issuer=BASE_AUTH_URL, expires_in=timedelta(minutes=30)):
     payload = user.copy()
     now = datetime.now(tz=timezone.utc)
+    payload["email"] = "easteregg@propelauth.com"
     payload["iat"] = now
     payload["exp"] = now + expires_in
     payload["iss"] = issuer
@@ -24,6 +25,7 @@ def random_org(user_role_str, permissions=None):
     return {
         "org_id": str(uuid4()),
         "org_name": str(uuid4()),
+        "org_metadata": {},
         "user_role": user_role_str,
         "inherited_user_roles_plus_current_role": [user_role_str],
         "user_permissions": [] if permissions is None else permissions,
