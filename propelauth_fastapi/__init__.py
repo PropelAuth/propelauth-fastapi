ea58c32709abdff1949dba2ebb0f080a2fa3d30b
pfvatterott <pfvatterott@gmail.com>
2025-05-21 14:19:58 -0600

    Added option to bring your own httpx client

--- propelauth_fastapi/__init__.py
+++ propelauth_fastapi/__init__.py
@@ -1,3 +1,4 @@
+import httpx
 from typing import Any, Dict, List, Optional
 from fastapi import Depends, HTTPException, Header
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
@@ -460,6 +461,7 @@ class FastAPIAuthAsync(FastAPIAuth):
         integration_api_key: str, 
         token_verification_metadata: Optional[TokenVerificationMetadata], 
         debug_mode: bool,
+        httpx_client: Optional[httpx.AsyncClient] = None,
     ):
         super().__init__(
             auth_url = auth_url, 
@@ -468,7 +470,21 @@ class FastAPIAuthAsync(FastAPIAuth):
             debug_mode = debug_mode
         )
         
+        self.is_httpx_client_provided = httpx_client is not None
+        if httpx_client:
+            self.httpx_client = httpx_client
+        else:
+            self.httpx_client = httpx.AsyncClient()
+            self.is_httpx_client_provided = False
+        
         self.auth = init_base_async_auth(auth_url, integration_api_key, token_verification_metadata)
+        
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type=None, exc_val=None, exc_tb=None):
+        if not self.is_httpx_client_provided:
+            await self.httpx_client.aclose()
         
     async def fetch_user_metadata_by_user_id(self, user_id: str, include_orgs: bool = False):
         return await self.auth.fetch_user_metadata_by_user_id(user_id, include_orgs)
@@ -773,7 +789,8 @@ def init_auth_async(
     api_key: str,
     token_verification_metadata: Optional[TokenVerificationMetadata] = None,
     debug_mode=False,
+    httpx_client: Optional[httpx.AsyncClient] = None,
 ) -> FastAPIAuthAsync:
     """Fetches metadata required to validate access tokens and returns auth decorators and utilities"""
-    return FastAPIAuthAsync(auth_url=auth_url, integration_api_key=api_key, token_verification_metadata=token_verification_metadata, debug_mode=debug_mode)
+    return FastAPIAuthAsync(auth_url=auth_url, integration_api_key=api_key, token_verification_metadata=token_verification_metadata, debug_mode=debug_mode, httpx_client=httpx_client)
     
